ccd599b43ae6c68be2e53e2c125b78b809c48013
Kaparovvv <kaparovkanagat@gmail.com>
2023-02-15 00:23:11 +0600

    main screen

---
+++ lib/commons/images_helper.dart
@@ -0,0 +1,7 @@
+class ImageHelper {
+  static const String background = 'assets/images/background.png';
+  static const String background1 = 'assets/images/background1.png';
+  static const String background2 = 'assets/images/background2.png';
+  static const String background3 = 'assets/images/background3.png';
+  static const String background4 = 'assets/images/background4.png';
+}
--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,12 +1,10 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:selim_trade_app/feature/main/presentation/main_screen.dart';
-import 'package:selim_trade_app/feature/splash/splash_screen.dart';
 
 @MaterialAutoRouter(
   replaceInRouteName: 'Page Route',
   routes: <AutoRoute>[
-    AutoRoute(path: 'splashScreen', page: SplashScreen, initial: true),
-    AutoRoute(path: 'mainScreen', page: MainScreen),
+    AutoRoute(path: 'mainScreen', page: MainScreen, initial: true),
   ],
 )
 class $AppRouter {}
